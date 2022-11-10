ffdab54e090e03c24e0dfb118d569ca6181959f2
YuleiYan <yuleiyan.yy@proton.me>
2022-11-10 21:06:20 +0100

    Add activity básica del juego

--- app/src/main/java/com/example/piedrapapeltijeras/ActivityMenuInicio.java
+++ app/src/main/java/com/example/piedrapapeltijeras/ActivityMenuInicio.java
@@ -2,27 +2,35 @@ package com.example.piedrapapeltijeras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class ActivityMenuInicio extends AppCompatActivity {
     Button bEntrar, bSalir;
+    EditText etNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu_inicio);
         inicializarVariables();
     }
 
     private void inicializarVariables() {
         bEntrar = findViewById(R.id.bEntrar);
         bSalir = findViewById(R.id.bSalir);
+        etNombre = findViewById(R.id.etNombre);
     }
 
     public void entrar(View view) {
+        Intent intent1 = new Intent(view.getContext(), ActivityJuego.class);
 
+        intent1.putExtra("res1", etNombre.getText().toString());
+
+        startActivityForResult(intent1, 0);
     }
 
     public void salir(View view) {
